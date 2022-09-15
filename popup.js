8deb1666441678b021bcbe19ab92c6a47f1deb70
Subrata Sarker <subrata_sarker@yahoo.com>
2022-09-15 18:29:22 +0600

    Style the status to make it look better with/without buttons

--- popup.js
+++ popup.js
@@ -1,22 +1,14 @@
 import { getActiveTabURL } from "./utils.js";
 
-const viewStats = (currentArticles) => {
-    const domStats = document.getElementsByClassName("articles")[0];
-    const totalArticles = 0;
-    Object.keys(currentArticles).forEach(page => {
-        totalArticles += currentArticles[page].length;
-    });
-    domStats.innerText = totalArticles;
-};
 
 document.addEventListener("DOMContentLoaded", async () => {
     const activeTab = await getActiveTabURL();
     if (activeTab.url.includes("https://scholar.google.com/scholar")) {
         const queryParameters = activeTab.url.split("?")[1];
         const urlParameters = new URLSearchParams(queryParameters);
-        const pageNumber = Number(urlParameters.get("start")) / 10 + 1;
+        const currentPageNumber = Number(urlParameters.get("start")) / 10 + 1;
         const queryParam = urlParameters.get("q");
-        if (pageNumber && queryParam) {
+        if (currentPageNumber && queryParam) {
             const domContainer = document.getElementsByClassName("container")[0]
             const domButtons = document.createElement('div');
             const btnNames = {
@@ -35,11 +27,10 @@ document.addEventListener("DOMContentLoaded", async () => {
             domContainer.append(domButtons);
             chrome.storage.sync.get([queryParam], (data) => {
                 const currentArticles = data[queryParam] ? JSON.parse(data[queryParam]) : {};
-                viewStats(currentArticles);
             });
         }
     } else {
         const status = document.getElementsByClassName("status")[0];
-        status.innerHTML = '<h3 class="status-message">This is not Google Scholar page for articles</h3>';
+        status.innerHTML = '<p class="status-message">This is not Google Scholar page for articles!</p>';
     }
 });
