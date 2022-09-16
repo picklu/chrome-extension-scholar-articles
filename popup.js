b2e0a0cf03bf0b391b601b26b9658e1cec104dfc
Subrata Sarker <subrata_sarker@yahoo.com>
2022-09-16 18:24:14 +0600

    Use utility function getQueryParams to extract page number and query

--- popup.js
+++ popup.js
@@ -1,24 +1,26 @@
-import { getActiveTabURL } from "./utils.js";
+import { getActiveTabURL, getQueryParams } from "./utils.js";
 
 
 document.addEventListener("DOMContentLoaded", async () => {
+
     const activeTab = await getActiveTabURL();
     const domStatus = document.getElementById("status");
     const domArticles = document.getElementById("articles");
     const domButtons = document.getElementById('btns');
+
     if (activeTab.url.includes("https://scholar.google.com/scholar")) {
+
         domButtons.className = "btns btns-active";
-        const queryParameters = activeTab.url.split("?")[1];
-        const urlParameters = new URLSearchParams(queryParameters);
-        const pageNumber = Number(urlParameters.get("start")) / 10 + 1;
-        const queryParam = urlParameters.get("q");
+        const { pageNumber, queryParam } = getQueryParams(activeTab);
+
         if (pageNumber && queryParam) {
             chrome.storage.sync.get(["__google_scholar_search_result"], (data) => {
-                const currentSearchresults = data["__google_scholar_search_result"]["searchResult"];
+
                 const { currentPageNumber,
                     currentTotalResults,
                     currentTotalPages,
-                    currentQueryParam } = currentSearchresults;
+                    currentQueryParam } = data["__google_scholar_search_result"]["searchResult"];
+
                 if (currentTotalResults && queryParam === currentQueryParam) {
                     domArticles.innerText = `[${currentTotalResults}] Page ${currentPageNumber} of ${currentTotalPages}`
                 } else {
--- utils.js
+++ utils.js
@@ -7,3 +7,12 @@ export async function getActiveTabURL() {
 
     return tabs[0];
 }
+
+export function getQueryParams(activeTab) {
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+    const pageNumber = Number(urlParameters.get("start")) / 10 + 1;
+    const queryParam = urlParameters.get("q");
+
+    return { pageNumber, queryParam };
+}
