3a86cc3e0bff750e43cce9280db38f111903b9a8
Subrata Sarker <subrata_sarker@yahoo.com>
2022-09-20 14:33:20 +0600

    Check pageNumber and SearchKey in the background script

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 const storageKey = "__google_scholar_search_result";
 const searchResutlKey = "searchResult";
 const articlesKey = "articles";
+const articlesPerPage = 20;
 const initialData = {
   [storageKey]: {
     [searchResutlKey]: {},
@@ -16,8 +17,12 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && tab.url && tab.url.includes("https://scholar.google.com/scholar")) {
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
-    const pageNumber = Number(urlParameters.get("start") || "0") / articlesPerPage + 1;
-    const searchKey = urlParameters.get("q");
+
+    let pageNumber = Number(urlParameters.get("start") || "0") / articlesPerPage + 1;
+    let searchKey = urlParameters.get("q");
+
+    pageNumber = pageNumber ? pageNumber : 0;
+    searchKey = searchKey ? searchKey : "cited articles";
 
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
