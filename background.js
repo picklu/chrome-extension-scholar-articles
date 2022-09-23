c22ceafed5b702cf1a9b58addea825ea64772f29
Subrata Sarker <subrata_sarker@yahoo.com>
2022-09-23 20:10:23 +0600

    wrap the whole background script inside a self invoking function

--- background.js
+++ background.js
@@ -1,23 +1,25 @@
-const storageKey = "__google_scholar_search_result";
-const searchResutlKey = "searchResult";
-const articlesKey = "articles";
-const articlesPerPage = 20;
-const initialData = {
-  [storageKey]: {
-    [searchResutlKey]: {},
-    [articlesKey]: {}
+(() => {
+  const storageKey = "__google_scholar_search_result";
+  const searchResutlKey = "searchResult";
+  const articlesKey = "articles";
+  const initialData = {
+    [storageKey]: {
+      [searchResutlKey]: {},
+      [articlesKey]: {}
+    }
   }
-}
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set(initialData);
-});
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set(initialData);
+  });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url && tab.url.includes("https://scholar.google.com/scholar")) {
-    chrome.tabs.sendMessage(tabId, {
-      type: "NEW"
-    });
-  }
-});
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete" &&
+      tab.url && tab.url.includes("https://scholar.google.com/scholar")) {
+      chrome.tabs.sendMessage(tabId, {
+        type: "NEW"
+      });
+    }
+  });
 
+})();
